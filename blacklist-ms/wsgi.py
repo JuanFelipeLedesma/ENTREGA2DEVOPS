175aff9b0b4105d78d1f0d5ea4e4d56316949b3e
Juan Felipe Ledesma Velasquez <111008847+JuanFelipeLedesma@users.noreply.github.com>
2025-11-27 14:50:27 -0500

    typo fix

--- blacklist-ms/wsgi.py
+++ blacklist-ms/wsgi.py
@@ -1,4 +1,4 @@
-from application import application  # o desde donde tengas tu instancia 'app'
+from application import app  # o desde donde tengas tu instancia 'app'
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
